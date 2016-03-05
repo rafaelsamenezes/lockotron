964ce27ab0641d730179c9c77d512970354f57c4
Rafael Sá <rafa.sa.xp@gmail.com>
2016-03-05 18:13:09 -0400

    Added GlobalVariables to shared vars

--- Galileo/GlobalVariables.py
+++ Galileo/GlobalVariables.py
@@ -1,4 +1,8 @@
-
 server_url = "http://localhost/lockotron/public_html"
-frame = 'frame.jpg'
-update = 'new-faces.xml'
+
+eigenface_model = 'faces.xml'
+frame           = 'frame.jpg'
+update          = 'new-faces.xml'
+
+doorGPPort = 13
+gp_path    = "/sys/class/gpio/"
