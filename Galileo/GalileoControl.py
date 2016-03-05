43e8c1e86ca64307cbe4f5709ff38b6b4d74914d
Rafael Sá <rafa.sa.xp@gmail.com>
2016-03-05 15:54:45 -0400

    Added static methods

--- Galileo/GalileoControl.py
+++ Galileo/GalileoControl.py
@@ -1,8 +1,33 @@
-class GalileoControl:
-    'This class is responsible for handling gpio from galileo'
+from GalileoNetwork import GalileoNetwork as GN
+import os.path
+import GlobalVariables as gv
 
+class GalileoControl:
+    'This class is responsible for handling gpio and system operations from galileo'
+    @staticmethod
     def openDoor():
         return 0
 
+    @staticmethod
     def updateSystem():
+        network = GN(gv.server_url)
+        update = network.getFaces()
+        GalileoControl.createFile('new-faces.xml', update)
         return 0
+
+    @staticmethod
+    def getFrame():
+        network = GN(gv.server_url)
+        frame = network.getFrame()
+        GalileoControl.createFile('frame.jpg', frame)
+
+    @staticmethod
+    def createFile(file_name, contents):
+        fo = open(file_name, 'wb')
+        fo.write(contents)
+        fo.close()
+
+    @staticmethod
+    def isUpdateAvailable():
+        check = os.path.exists('new-faces.xml')
+        return check
