ec4c62f140615db59965a9919c8fabb8fd41efe5
Rafael Sá <rafa.sa.xp@gmail.com>
2016-03-05 20:56:30 -0400

    Updated server

--- Galileo/GalileoUDP.py
+++ Galileo/GalileoUDP.py
@@ -12,6 +12,6 @@ sock.bind(("", udp_receive_port))
 while True:
     data, addr = sock.recvfrom(1024)  # tamanho do buffer 1024 bytes
     if (data == 0):
-        GalileoControl.openDoor
+        GalileoControl.openDoor()
     elif (data == 1):
-        GalileoControl.updateSystem
+        GalileoControl.updateSystem()
