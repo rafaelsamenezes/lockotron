8e9b9821909089e9e02815fd9d7c954e7c580b8f
Rafael Sá <rafa.sa.xp@gmail.com>
2016-03-05 20:57:05 -0400

    Created class and mais procedures

--- Galileo/RecognizeFace.py
+++ Galileo/RecognizeFace.py
@@ -1,11 +1,17 @@
-import numpy as np
-import GlobalVariables as gv
 from GalileoControl import GalileoControl as GC
 from GalileoNetwork import GalileoNetwork as GN
+import numpy as np
+import GlobalVariables as gv
 import cv2
 
-GC.getFrame()
-img = cv2.imread(gv.frame, 0)
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+class RecognizeFace:
+    model = 0
+
+    def __init__(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def isGoodPrediction(self, person_id):
+        pass
