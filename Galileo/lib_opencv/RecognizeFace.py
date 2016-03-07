922bf1f4f983ee14d18ab043451401f5dfb2ac50
Rafael Sá <rafa.sa.xp@gmail.com>
2016-03-07 08:26:36 -0400

    Added support to facerecognizer algorithms

--- Galileo/lib_opencv/RecognizeFace.py
+++ Galileo/lib_opencv/RecognizeFace.py
@@ -1,17 +1,39 @@
-from GalileoControl import GalileoControl as GC
-from GalileoNetwork import GalileoNetwork as GN
 import numpy as np
 import GlobalVariables as gv
 import cv2
 
 class RecognizeFace:
-    model = 0
 
-    def __init__(self):
-        pass
+    def __init__(self, num):
+        if (num == 1):
+            self.model = cv2.face.createEigenFaceRecognizer()
+        elif (num == 2):
+            self.model = cv2.face.createFisherFaceRecognizer()
+        else:
+            self.model = cv2.face.createLBPHFaceRecognizer()
 
-    def predict(self):
-        pass
+    def load(self, file):
+        self.model.load(file)
 
-    def isGoodPrediction(self, person_id):
-        pass
+    def save(self, file):
+        self.model.save(file)
+
+    def train(self, num_labels, path, num_img):
+        images = list()
+        labels = list()
+        for i in num_labels:
+            for j in range(0, num_img):
+                img = cv2.imread(path+str(i)+'/frame'+str(j)+'.jpg')
+                images.append(img)
+                labels.append(int(i))
+        l = np.array(labels)
+        self.model.train(images, l)
+
+    def predict(self, frame):
+        f = cv2.imread(frame, cv2.COLOR_BGR2GRAY)
+        return self.model.predict(f)
+
+    def isGoodPrediction(self, frame):
+        W = self.model.getEigenVectors()
+        mean = self.model.getMean()
+        face = cv2.imread(frame, cv2.COLOR_BGR2GRAY)
