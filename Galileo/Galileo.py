6c933a7783a6af3d81964eb3fe1a1755a5b6853c
Rafael Sá <rafa.sa.xp@gmail.com>
2016-03-08 08:11:37 -0400

    Galileo: Added thread support to main script

--- Galileo/Galileo.py
+++ Galileo/Galileo.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python
 from lib_galileo.GalileoControl import GalileoControl as gc
+from lib_galileo.GalileoControl import Lock_o_tron as loc
 from lib_galileo.GalileoNetwork import GalileoNetwork as gn
 from lib_opencv.RecognizeFace import RecognizeFace as rf
 import GlobalVariables as gv
+import GalileoUDP as udp_thread
+import GalileoFaceRecognizer as fr_thread
 import time
 
-def checkUpdate():
-    if(gc.isUpdateAvailable()):
-        gc.renameFile(gv.update, gv.model)
-
-def checkPerson(person_id):
-    conn = gn.GalileoNetwork(gv.server_url)
-    return conn.askPermission(person_id)
-
-def setupGPIO():
+def setupGPIO(self):
     gc.gpio_export(gv.doorGPPort)
-    gc.gpio_export(gv.motionDetector)
-    gc.gpio_export(gv.servo)
     gc.gpio_set_mode(gv.doorGPPort, gc.INPUT)
-    gc.gpio_set_mode(gv.motionDetector, gc.OUTPUT)
-    gc.gpio_set_mode(gv.servo, gc.OUTPUT)
-
-def checkMotion():
-    value = gc.gpio_get_value(gv.motionDetector)
-    return value
 
 if __name__ == '__main__':
-    checkUpdate()
     setupGPIO()
-    recognizer = rf.RecognizeFace()
-    while(True):
-        time.sleep(1)
-        if(checkMotion == gc.HIGH):
-            gc.getFrame()
-            prediction = rf.predict()
-            if (rf.isGoodPrediction(prediction) and checkPerson(prediction)):
-                gc.openDoor()
+    t1, t2 = udp_thread.UDPThread(), fr_thread.GalileoFaceRecognizerThread()
+    t1.start()
+    t1.daemon = True
+    while True:
+        if (gc.gpio_get_value(gv.motionDetector == gc.HIGH) and (not t2.is_alive())):
+            pass
+        elif (t1.isMessageReceived()):
+            pass
