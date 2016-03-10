16684cbeefa1a74715f6836e11c527139d86001b
Rafael Sá <rafa.sa.xp@gmail.com>
2016-03-09 20:31:40 -0400

    Galileo: Face is now recognized using another proccess (using system)

--- Galileo/lib_opencv/RecognizeFace.py
+++ Galileo/lib_opencv/RecognizeFace.py
@@ -1,39 +1,15 @@
-import numpy as np
-import GlobalVariables as gv
-import cv2
+import os
 
+frame = 'frame.jpg'
 class RecognizeFace:
 
-    def __init__(self, num):
-        if (num == 1):
-            self.model = cv2.face.createEigenFaceRecognizer()
-        elif (num == 2):
-            self.model = cv2.face.createFisherFaceRecognizer()
-        else:
-            self.model = cv2.face.createLBPHFaceRecognizer()
+    @staticmethod
+    def predict():
+        print 'Verificando pessoa...'
+        prediction = os.system('./facerecognizer ../{frame}'.format(frame=frame))
+        good = RecognizeFace.__isGoodPrediction()
+        return int(prediction), good
 
-    def load(self, file):
-        self.model.load(file)
-
-    def save(self, file):
-        self.model.save(file)
-
-    def train(self, num_labels, path, num_img):
-        images = list()
-        labels = list()
-        for i in num_labels:
-            for j in range(0, num_img):
-                img = cv2.imread(path+str(i)+'/frame'+str(j)+'.jpg')
-                images.append(img)
-                labels.append(int(i))
-        l = np.array(labels)
-        self.model.train(images, l)
-
-    def predict(self, frame):
-        f = cv2.imread(frame, cv2.COLOR_BGR2GRAY)
-        return self.model.predict(f)
-
-    def isGoodPrediction(self, frame):
-        W = self.model.getEigenVectors()
-        mean = self.model.getMean()
-        face = cv2.imread(frame, cv2.COLOR_BGR2GRAY)
+    @staticmethod
+    def __isGoodPrediction():
+        return True
