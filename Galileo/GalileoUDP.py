b9f27864c03347f4691f48161a558989c38b96ee
Rafael Sá <rafa.sa.xp@gmail.com>
2016-03-09 20:27:05 -0400

    Galileo: Added log messages to UDP server

--- Galileo/GalileoUDP.py
+++ Galileo/GalileoUDP.py
@@ -6,6 +6,7 @@ import threading
 class UDPThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
+        print('Inicializando thread de UDP')
         self.gotMessage = False
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(("", gv.udp_receive_port))
@@ -14,9 +15,11 @@ class UDPThread(threading.Thread):
         while True:
             data, addr = self.sock.recvfrom(1024)
             if (data == 0):
+                print('Servidor UDP recebeu mensagem de abrir porta')
                 self.gotMessage = True
                 loc.openDoor()
             elif (data == 1):
+                print('Servidor UDP recebeu mensagem de atualização')
                 self.gotMessage = True
                 loc.updateSystem()
 
