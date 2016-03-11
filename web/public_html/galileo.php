edfb4e0afc1d76f22e61eb6840a025c96ca15973
Rodrigo Tavares <rodrigost23@gmail.com>
2016-03-11 15:25:46 -0400

    Web: Add kill command

--- web/public_html/galileo.php
+++ web/public_html/galileo.php
@@ -1,10 +1,12 @@
 <?php
-$galileo_ip = "127.0.0.1";
+$galileo_ip = "10.42.0.252";
 
 if (isset($_GET['panic']) || isset($_GET['panic'])) {
     $msg = 0;
 } elseif (isset($_GET['update']) || isset($_GET['update'])) {
     $msg = 1;
+} elseif (isset($_GET['kill']) || isset($_GET['kill'])) {
+    $msg = 3;
 } else {
     http_response_code(500);
     exit();
