f39815fb6aed463da2ea0194db731bfa26b3bcf1
Rodrigo Tavares <rodrigost23@gmail.com>
2016-03-14 19:26:36 -0400

    Mobi-o-tron: Adjusting icons and menu

--- Mobi-o-tron/app/src/main/java/com/lockotron/mobi_o_tron/MainActivity.java
+++ Mobi-o-tron/app/src/main/java/com/lockotron/mobi_o_tron/MainActivity.java
@@ -86,17 +86,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_log) {
+            // Handle the log action
+        } else if (id == R.id.nav_stats) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_control) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
