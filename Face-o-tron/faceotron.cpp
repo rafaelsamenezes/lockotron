3fc5818efc9f4598683cd74d28b5b77cb5b230ad
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2016-03-17 15:18:04 -0400

    Removed Face-o-tron lib

--- Face-o-tron/faceotron.cpp
+++
@@ -1,6 +0,0 @@
-#include "faceotron.h"
-
-
-Faceotron::Faceotron()
-{
-}
--- Face-o-tron/haarcascade.cpp
+++
@@ -1,5 +0,0 @@
-#include "haarcascade.h"
-
-string HaarCascade::faceHaarCascade = "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml";
-string HaarCascade::eyeHaarCascade = "/usr/local/share/OpenCV/haarcascades/haarcascade_eye.xml";
-string HaarCascade::glassesHaarCascade = "/usr/local/share/OpenCV/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
